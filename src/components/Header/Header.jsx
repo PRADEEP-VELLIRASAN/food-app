83d600cee4532cecaabde66b632b4e4fd9b7d607
PRADEEP V <geethapradeep243@gmail.com>
2025-08-01 14:15:43 +0530

    added comment

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,20 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
+      {/* Container for header text and button */}
       <div className="header-contents">
+        {/* Main heading */}
         <h2>Order Your Favourite Food Here</h2>
+        
+        {/* Descriptive paragraph */}
         <p>
           Choose from a diverse menu featuring a delectable array of dishes
           crafted with the finest ingredients and culinary expertise. Our
           mission is to satisfy your cravings and elevate your dining
           experience, one delicious meal at a time.
         </p>
+        
+        {/* Call-to-action button wrapped in anchor to navigate to menu section */}
         <a href="#explore-menu">
           <button>View Menu</button>
         </a>
